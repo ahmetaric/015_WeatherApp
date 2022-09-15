8bf62eef5282a930c23ecda7270809a821f538f2
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-15 09:48:19 +0300

    first commit

---
+++ weatherapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from decouple import config
+from pprint import pprint
+import requests
+
+def index(request):
+    city = "Yozgat"
+    API_KEY = config("API_KEY")
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    content = response.json()
+    # pprint(content)
+    pprint(content["name"])
+    pprint(content["main"]["temp"])
+    pprint(content["weather"][0]["description"])
+    pprint(content["weather"][0]["icon"])
+
+
+    context = {
+       "city" :  content["name"],
+       "temp" :  content["main"]["temp"],
+       "icon" :  content["weather"][0]["icon"],
+       "desc" :  content["weather"][0]["description"],
+
+    }
+    return render(request, 'weatherapp/index.html',context)
+
+
+
